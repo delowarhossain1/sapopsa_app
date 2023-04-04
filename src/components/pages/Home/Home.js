f081474c94019feff3cdf78adb220ecceb934f71
DeveloperDelowar <programmerdaloar@gmail.com>
2023-04-04 06:31:29 -0400

    Loading status component added

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,6 +2,12 @@ import React from 'react';
 import PageTitle from '../../shared/PageTitle/PageTitle';
 
 const Home = () => {
+    const [loading, setLoading] = useState(false);
+
+    // Set loading status;
+    if(loading) <Loading />;
+
+    
     return (
         <div>
             <PageTitle text='Home' />
